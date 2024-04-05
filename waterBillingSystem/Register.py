065c4d79891a103deb9ade046735f9b9b9335af8
Savindu <savinduruhunuhewa@gmail.com>
2024-04-05 16:44:00 +0530

    Update the Register page

--- waterBillingSystem/Register.py
+++ waterBillingSystem/Register.py
@@ -3,5 +3,11 @@ import mysql.connector
 
 
 register=Tk();
+register.title("Register")
+register.resizable(False,False)
+register.geometry("400x300+600+150")
+
+label1 =Label(register, text="FirstName")
+label1.
 register.mainloop()
 
